ae902d101a88ffc47ede26d21033c3f0d3eadefc
Eugen1188 <eugen.ferchow@gmx.de>
2025-03-22 00:51:53 +0100

    add_edit_fnctn

--- src/app/chat-content/channel-edit/channel-edit.component.ts
+++ src/app/chat-content/channel-edit/channel-edit.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { doc, updateDoc } from '@angular/fire/firestore';
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-channel-edit',
@@ -11,25 +13,61 @@ export class ChannelEditComponent {
   @Input() channelInfo:boolean = false;
   @Input() currentChannel:any = {};
 
-  @Output() channelInfoChange = new EventEmitter<boolean>();  channelnameEdit: boolean = false;
+  @Output() channelInfoChange = new EventEmitter<boolean>();
+  channelnameEdit: boolean = false;
   channeldescriptionEdit: boolean = false;
+
+  constructor(public firestore: Firestore) {}
 
   editChannelName(content: string) {
     console.log(content);
     if (content == 'editName') {
       if (!this.channelnameEdit) {
         this.channelnameEdit = true;
-      } else this.channelnameEdit = false;
+      } else {
+        this.channelnameEdit = false;
+        this.saveNewChannelData(content);
+      } 
     }
     if (content == 'editDescription') {
       if (!this.channeldescriptionEdit) {
         this.channeldescriptionEdit = true;
-      } else this.channeldescriptionEdit = false;
+      } else {
+        this.channeldescriptionEdit = false;
+        this.saveNewChannelData(content);
+      } 
     }
   }
 
-  saveNewChannelData() {
-    console.log('Neuer Channelbeschreibung gespeichert');
+  async saveNewChannelData(content:string) {
+    const newChannelName = document.getElementById('changeNameInput') as HTMLInputElement;
+    const newChannelDescription = document.getElementById('changeDescriptionInput') as HTMLInputElement;
+    const ChannelNameRef = doc(this.firestore, 'channels', this.currentChannel.id);
+    // const newChannelDescriptionRef = doc(this.firestore, 'channels', this.currentChannel.id)
+    console.log(content);
+    try {
+      if(content == 'editName') {
+        const ChannelNameRef = doc(this.firestore, 'channels', this.currentChannel.id);
+
+        await updateDoc(ChannelNameRef, {
+          name: newChannelName.value
+        })
+        this.currentChannel.name = newChannelName.value;
+
+      }
+      if(content == 'editDescription') {
+        const ChannelNameRef = doc(this.firestore, 'channels', this.currentChannel.id);
+
+        await updateDoc(ChannelNameRef, {
+          description: newChannelDescription.value
+        })
+        this.currentChannel.description = newChannelDescription.value;
+
+      }
+    } catch (error) {
+      console.error('Input Elemente nicht gefunden');
+      
+    }
   }
 
   adjustTextareaHeight(event: Event) {
@@ -43,6 +81,9 @@ export class ChannelEditComponent {
   close() {
     this.channelInfo = false;
     this.channelInfoChange.emit(this.channelInfo);
-    console.log(this.channelInfo);
+  }
+
+  exitChannel() {
+    
   }
 }
