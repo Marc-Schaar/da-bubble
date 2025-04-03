c72a80f707d5a6002abb5a3ba5e308f3c77384aa
Marc Schaar <m-schaar@gmx.net>
2025-04-03 19:12:22 +0200

    bugfix

--- src/app/thread/thread.component.ts
+++ src/app/thread/thread.component.ts
@@ -1,83 +1,80 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UserService } from '../shared.service';
 import { FireServiceService } from '../fire-service.service';
-import { onSnapshot, QuerySnapshot } from '@angular/fire/firestore';
+import { Firestore, doc, getDoc, collection, onSnapshot } from '@angular/fire/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-thread',
   templateUrl: './thread.component.html',
   styleUrls: ['./thread.component.scss'],
 })
-export class ThreadComponent implements OnInit, OnDestroy {
-  unsubMessages!: () => void;
-  urlDataSubscription!: Subscription;
-
+export class ThreadComponent implements OnInit {
   userService: UserService = inject(UserService);
   fireService: FireServiceService = inject(FireServiceService);
   route: ActivatedRoute = inject(ActivatedRoute);
   router: Router = inject(Router);
+  firestore: Firestore = inject(Firestore); // ✅ Richtige Firestore-Instanz nutzen
 
   currentChannelId: string = '';
   parentMessageId: string = '';
   parentMessageData: any = null;
   isMobile: boolean = false;
+  unsubMessages!: () => void;
 
   ngOnInit(): void {
-    this.setUrlData();
+    this.route.queryParams.subscribe((params) => {
+      this.currentChannelId = params['id'] || '';
+      this.parentMessageId = params['messageId'] || '';
+      this.getThreadParentMessage();
+    });
+
     this.userService.screenWidth$.subscribe((isMobile) => {
       this.isMobile = isMobile;
       console.log('Ist Mobile Ansicht aktiv?:', this.isMobile);
     });
   }
 
-  ngOnDestroy(): void {
-    if (this.urlDataSubscription) {
-      this.urlDataSubscription.unsubscribe();
-    }
-    if (this.unsubMessages) {
-      this.unsubMessages();
+  async getThreadParentMessage() {
+    try {
+      let parentMessageDocRef = doc(this.firestore, `channels/${this.currentChannelId}/messages/${this.parentMessageId}`);
+      let parentMessageDocSnap = await getDoc(parentMessageDocRef);
+
+      if (parentMessageDocSnap.exists()) {
+        let data = parentMessageDocSnap.data();
+        this.setParentMessageData(data);
+      }
+    } catch (error) {
+      console.error(' Fehler beim Abrufen der Parent Message:', error);
     }
   }
 
-  setUrlData() {
-    this.urlDataSubscription = this.route.queryParams.subscribe((params) => {
-      this.currentChannelId = params['id'] || '';
-      this.parentMessageId = params['messageId'] || '';
-
-      this.getThreadParentMessage();
+  setParentMessageData(data: any) {
+    let parentMessage = data;
+    let formattedTime = new Date(parentMessage?.timestamp.toDate()).toLocaleTimeString('de-DE', {
+      hour: '2-digit',
+      minute: '2-digit',
     });
-  }
 
-  getThreadParentMessage() {
-    let threadRef = this.fireService.getCollectionRef(`channels/${this.currentChannelId}/messages/${this.parentMessageId}/thread`);
-
-    if (threadRef) {
-      this.unsubMessages = onSnapshot(threadRef, (snapshot) => {
-        if (!snapshot.empty) {
-          let doc = snapshot.docs[0];
-          let data = doc.data();
-          this.parentMessageData = {
-            id: doc.id,
-            ...data,
-            time: new Date(data['parentMessage']?.timestamp.toDate()).toLocaleTimeString('de-DE', {
-              hour: '2-digit',
-              minute: '2-digit',
-            }),
-          };
-        }
-      });
-    }
+    this.parentMessageData = {
+      id: this.parentMessageId,
+      ...parentMessage,
+      time: formattedTime,
+    };
   }
 
   closeThread() {
-    {
-      if (this.isMobile)
-        this.router.navigate(['/chat'], {
-          queryParams: { channelType: 'channel', id: this.currentChannelId, reciepentId: this.userService.docId },
-        });
-      this.userService.toggleThread();
+    if (this.isMobile) {
+      this.router.navigate(['/channel'], {
+        queryParams: { channelType: 'channel', id: this.currentChannelId, reciepentId: this.userService.docId },
+      });
+    }
+    this.userService.toggleThread();
+  }
+
+  ngOnDestroy(): void {
+    if (this.unsubMessages) {
+      this.unsubMessages();
     }
   }
 }
