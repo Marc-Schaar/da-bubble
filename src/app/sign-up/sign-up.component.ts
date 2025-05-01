633d1fd902b0ac2231a6f9ccad2f7cdc5fb1a6b4
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-05-01 16:43:20 +0300

    sign up jsdoc

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -19,11 +19,25 @@ export class SignupComponent implements OnInit {
   Auth = inject(Auth);
   user = new User();
   checked = false;
+
+  /**
+   * Initializes the signup component and sets login status to false.
+   */
+
   ngOnInit(): void {
     this.sharedservice.login = false;
   }
+
+  /**
+   * Constructor for initializing the component with Firestore.
+   * @param firestore - The Firestore instance to interact with Firestore services.
+   */
   constructor(public firestore: Firestore) {}
 
+  /**
+   * Handles the form submission to create a new user account.
+   * @param useraccount - The form containing the user's account details.
+   */
   async onSubmit(useraccount: NgForm) {
     if (useraccount.valid) {
       console.log(this.user);
