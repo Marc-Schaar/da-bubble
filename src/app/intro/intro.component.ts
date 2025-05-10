7305c91ddaa2795c422c6bb92e206b3399a80c45
Eugen1188 <eugen.ferchow@gmx.de>
2025-05-10 21:04:23 +0200

    showIntroOnlyOneTime

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -15,14 +15,5 @@ export class IntroComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    // setTimeout(() => {
-    //   this.router.navigate(['/main']);
-    // }, 4000);
-    // setTimeout(() => {
-    //   const projectName = document.getElementById('main');
-    //   if (projectName) {
-    //     projectName.classList.add('d-none');
-    //   }
-    // }, 4000);
   }
 }
--- src/app/main-component/main-component.component.ts
+++ src/app/main-component/main-component.component.ts
@@ -5,28 +5,42 @@ import { SignInComponent } from '../sign-in/sign-in.component';
 import { UserService } from '../shared.service';
 import { IntroComponent } from '../intro/intro.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-component',
-  imports: [HeaderComponent, FooterComponent, SignInComponent, IntroComponent, RouterModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent, SignInComponent, IntroComponent, RouterModule],
   templateUrl: './main-component.component.html',
   styleUrl: './main-component.component.scss',
 })
 export class MainComponentComponent implements OnInit {
   shareddata = inject(UserService);
+  showIntro: boolean = true;
 
   /**
    * Lifecycle hook that is called when the component is initialized.
+   * checks if the intro should be shown based on local storage value.
    * Sets the dashboard and login properties of the shared service and hides the intro element after 4 seconds.
    */
   ngOnInit(): void {
+    const showIntroStored = localStorage.getItem('showIntro');
+    if (showIntroStored === 'false') {
+      this.showIntro = false;
+    } else {
+      this.showIntro = true;
+      setTimeout(() => {
+        const projectName = document.getElementById('intro');
+        if (projectName) {
+          projectName.classList.add('d-none');
+          this.turnOffIntro();
+        }
+      }, 4000);
+    }
     this.shareddata.dashboard = false;
     this.shareddata.login = true;
-    setTimeout(() => {
-      const projectName = document.getElementById('intro');
-      if (projectName) {
-        projectName.classList.add('d-none');
-      }
-    }, 4000);
+  }
+
+  turnOffIntro() {
+    localStorage.setItem('showIntro', 'false');
   }
 }
