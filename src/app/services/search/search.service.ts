70b98fc54147662c7bcd539083dce51605905a6f
Marc Schaar <m-schaar@gmx.net>
2025-05-13 21:48:53 +0200

    tag reveiver in textarea

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -10,11 +10,11 @@ export class SearchService {
   authService: Auth = inject(Auth);
   userService: UserService = inject(UserService);
 
-  private get isAnonymous(): boolean {
-    return this.authService.currentUser?.isAnonymous ?? true;
+  private isAnonymous(): boolean | undefined {
+    return this.authService.currentUser?.isAnonymous;
   }
 
-  private get userId(): string | undefined {
+  private userId(): string | undefined {
     return this.authService.currentUser?.uid;
   }
 
@@ -42,10 +42,10 @@ export class SearchService {
   public startSearch(input: string, searchCollection: 'channel' | 'user') {
     let searchInput = input.trim().toLowerCase();
     let result: any[] = [];
-    let channelsToSearch = this.isAnonymous
+    let channelsToSearch = this.isAnonymous()
       ? this.userService.channels.filter((channel: { key: string }) => channel.key === 'KqvcY68R1jP2UsQkv6Nz')
       : this.userService.channels.filter((channel: { data?: { member?: any[] } }) =>
-          channel.data?.member?.some((member: any) => member.id === this.userId)
+          channel.data?.member?.some((member: any) => member.id === this.userId())
         );
     if (searchCollection === 'channel') result = this.searchChannel(searchInput, channelsToSearch);
     else if (searchCollection === 'user') result = this.searchChannelMembersByName(searchInput, channelsToSearch);
