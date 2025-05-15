494f6dbdd5675026bf2137199bcf7285d6a613ce
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-05-15 23:13:18 +0300

    Update intro.component.ts

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,18 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss'],
 })
-export class IntroComponent implements OnInit {
-  /**
-   * Constructor for SomeComponent. Initializes the component with the Router service for navigation.
-   *
-   * @param router - Router service instance used to navigate between views or routes in the application.
-   */
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {}
-}
+export class IntroComponent {}
