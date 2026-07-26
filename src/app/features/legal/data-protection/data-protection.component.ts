cceac2302d1b73a8c485b518c3a295e4307689ca
m-schaar@gmx.net <m-schaar@gmx.net>
2026-07-26 11:39:07 +0200

    refactor: replace back button image with Material icon and reset global margins

--- src/app/features/legal/data-protection/data-protection.component.ts
+++ src/app/features/legal/data-protection/data-protection.component.ts
@@ -4,10 +4,11 @@ import { Location } from '@angular/common';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { CONTACT_EMAIL } from '../../../shared/constants';
 import { ButtonDirective } from '../../../shared/components/button/button.directive';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dataprotection',
-  imports: [HeaderComponent, ButtonDirective],
+  imports: [HeaderComponent, ButtonDirective, MatIconModule],
   templateUrl: './data-protection.component.html',
   styleUrl: './data-protection.component.scss',
 })
